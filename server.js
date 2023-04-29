cd2b9f4c132918b4d647aa8a0d96c753ff37c1cf
Dewey Watkins <deweywatkins77@gmail.com>
2023-04-28 21:29:21 -0400

    updated package.json

---
+++ server.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const express = require('express')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+const routes = require('./controllers')
+const helpers = require('./utils/helpers')
+const sequelize = require('./config/connection')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(routes)
+
+const PORT = process.env.PORT || 3001
+
+//configure view engine
+const hbs = exphbs.create({ helpers })
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars')
+
+//setup session
+const sess = {
+    secret: 'blogsesspass4Cookies',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  }
+
+app.use(session(sess))
+
+//sync sequelize and start listening
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'))
+})
