f1f9af74a984a819989f66dbfc82dbb2a910690e
Dewey Watkins <deweywatkins77@gmail.com>
2023-04-29 01:14:44 -0400

    added posts and replies models, updated connection.js

---
+++ models/Posts.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/connection')
+
+class Posts extends Model{}
+
+Posts.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    creator:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    createdDate:{
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  },
+  {
+    hooks: {},
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+)
+
+module.exports = Posts
---
+++ models/Replies.js
@@ -0,0 +1,35 @@
+const {Models, DataTypes} = require('sequelize')
+const sequelize = require('../config/connection')
+
+class Replies extends Models{}
+
+Replies.init(
+    {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        replyContent: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+        replyCreator:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        replyDate:{
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+      },
+      {
+        hooks: {},
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+      }
+)
