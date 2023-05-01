21b92d7cbd672205db009359950295a03a4cbdca
Dewey Watkins <deweywatkins77@gmail.com>
2023-04-30 20:54:38 -0400

    formatted date for posts in the homepage route query

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { Posts, Users } = require('../models')
+const { Sequelize } = require('sequelize')
 
 router.get('/', async (req, res) => {
   try {
@@ -8,6 +9,12 @@ router.get('/', async (req, res) => {
         model: Users,
         attributes: ['name']
       }],
+      attributes:[
+        'id',
+        'title',
+        'content',
+        [Sequelize.fn('DATE_FORMAT', Sequelize.col('created_date'), '%m-%d-%Y'), 'created_date']
+      ],
       order: [['created_date','DESC']]
     })
     console.log(rows)
@@ -18,6 +25,7 @@ router.get('/', async (req, res) => {
       logged_in:req.session.logged_in
     })
   } catch (err) {
+    console.log(err)
     res.status(500).json(err)
   }
 })
